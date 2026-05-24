4d927c6e64ec8cff5ce4c4c5222707ceca66b4ee
Owen Hochwald <62612083+owenHochwald@users.noreply.github.com>
2026-05-24 09:49:46 -0400

    Add: verifying-an-alien-dictionary - submission-3

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        lookup = {c:i for i, c in enumerate(order)}
+
+        # we can compare neighbors
+        # if ascending
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+
+            for j in range(len(w1)):
+                if j == len(w2):
+                    # length violation, w2 is shorter and should come before
+                    return False
+                
+                if w1[j] != w2[j]:
+                    if lookup[w1[j]] > lookup[w2[j]]:
+                        return False
+                    break
+        return True
