f9c8d24a6f9b8ebcc8a071245bb2399a7aaf1ee3
Owen Hochwald <62612083+owenHochwald@users.noreply.github.com>
2026-09-14 10:52:01 -0700

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        n = len(s)
+
+        # this needs to be n+1 so that we can have the first index be true and give us something to "jump off of"
+        # we can always segment something that has no chars
+
+        dp = [False] * (n+1)
+        dp[0] = True
+
+        for i in range(1, n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+        return dp[n]
+
+        
+        
